8a3138db4011a497fce19f8404002badf9a8da10
krehermann <keith.rehermann@protonmail.com>
2023-01-20 07:57:15 -0700

    Add tests for memstore, chain.GetBlockAt
    precursor to filling out the GetBlock support

---
+++ core/storage_test.go
@@ -0,0 +1,26 @@
+package core
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMemStore_Get(t *testing.T) {
+	ms := NewMemStore()
+
+	nPuts := 5
+	want := make([]*Block, nPuts)
+	for i := 0; i < nPuts; i++ {
+		b := randomBlockWithoutPreviousBlock(t, uint32(i))
+		want[i] = b
+		assert.NoError(t, ms.Put(b))
+
+		for j := 0; j <= i; j++ {
+			gotBlock, err := ms.Get(want[j].Header)
+			assert.NoError(t, err)
+			assert.Equal(t, want[j], gotBlock)
+
+		}
+	}
+}
